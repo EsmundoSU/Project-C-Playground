8097221b124cc8b892b4ce1093737f03317ed6c2
AdamPelcSU <adam.pelc.su@gmail.com>
2021-03-28 17:43:39 +0200

    CI - updated

--- core/main.c
+++ core/main.c
@@ -5,5 +5,5 @@ const char TestText1[] = "Hello World! APE\nWhere is my mind\n";
 
 int main() {
     size_t indexOfNewLine = NewLineIndex(TestText1, sizeof TestText1);
-    printf("indexOfNewLine - TestText1 is: %zu", indexOfNewLine);
+    printf("indexOfNewLine - TestText1 is: %u", (unsigned) indexOfNewLine);
 }
